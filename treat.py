51ce39ea13dca84606de53f669093b54b5fc6dbb
Valery <drobotun.mail@gmail.com>
2023-01-26 23:32:03 +0100

    NetworkX, very slow version

--- treat.py
+++ treat.py
@@ -1,73 +1,219 @@
 from PIL import Image
 from scipy import ndimage
 import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
+import graph_tool.all as gt
+import graph_tool.draw as gt_draw
+from datetime import datetime, timedelta
 import math
+import logging
 
-image = Image.open('banc-1.jpg').convert('L')
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
-grad_x = ndimage.sobel(image, axis=0, mode='constant')
-grad_y = ndimage.sobel(image, axis=1, mode='constant')
+def get_node_index(image, x, y):
+    return y * image.width + x
 
-grad_l1 = np.abs(grad_x) + np.abs(grad_y)
+def get_gradient_image(image):
+    grad_x = ndimage.sobel(image, axis=0, mode='constant')
+    grad_y = ndimage.sobel(image, axis=1, mode='constant')
 
-print(image.width, image.height)
+    return np.abs(grad_x) + np.abs(grad_y)
 
-G = nx.DiGraph()
+def build_graph(image):
+    print(datetime.now(), "Size:", image.width, image.height)
 
-for width in range(image.width):
+    G = gt.Graph()
+    G.add_vertex(image.width * image.height + 2)
+
+    return G
+
+def graph_add_weights(G, grad_l1):
+    back_weight = np.sum(grad_l1)
+
+    edge_list = []
+    for width in range(image.width):
+        for height in range(image.height):
+
+            current = G.vertex(get_node_index(image, width, height))
+            right = G.vertex(get_node_index(image, width + 1, height))
+
+            if width != image.width - 1: 
+                edge_list.append(
+                    (
+                        current,
+                        right,
+                        grad_l1[height][width]
+                    )
+                )
+                edge_list.append(
+                    (
+                        right,
+                        current,
+                        back_weight
+                    )
+                )
+
+            if width != 0 and height != image.height - 1:
+                edge_list.append(
+                    (
+                        current,
+                        G.vertex(get_node_index(image, width - 1, height + 1)),
+                        back_weight
+                    )
+                )
+
+            if width != image.width - 1 and height != image.height - 1:
+                edge_list.append(
+                    (
+                        G.vertex(get_node_index(image, width + 1, height + 1)),
+                        current,
+                        back_weight
+                    )
+                )
+
+    source = G.vertex(get_node_index(image, 0, image.height))
+    sink = G.vertex(get_node_index(image, 1, image.height))
+    
     for height in range(image.height):
-        G.add_node(height * image.width + width, pos=(width, height))
+        # connect first column
+        edge_list.append(
+            (
+                source,
+                G.vertex(get_node_index(image, 0, height)),
+                back_weight
+            )
+        )
 
-print(len(G.edges), len(G.nodes), image.width * image.height)
+        #connect last column
+        edge_list.append(
+            (
+                G.vertex(get_node_index(image, image.width - 1, height)),
+                sink,
+                back_weight
+            )
+        )
 
-source_index = image.width * image.height + 1
-dest_index = image.width * image.height + 2
-# Source 
-G.add_node(source_index)
-# Sink
-G.add_node(dest_index)
+    weight = G.new_edge_property("int")
+    eprops = [weight]
+    G.add_edge_list(edge_list, eprops=eprops)
+    G.edge_properties["weight"] = weight
 
-back_weight = np.sum(grad_l1)
+    return G
 
-for width in range(image.width - 1):
-    print(f"Width {width} out of {image.width}")
-    for height in range(image.height):
-        G.add_edge(height * image.width + width, height * image.width + width + 1, capacity=grad_l1[height][width])
-        G.add_edge(height * image.width + width + 1, height * image.width + width, capacity=back_weight)
 
-        if height % 2 == 0 and height != image.height - 1:
-            G.add_edge(height * image.width + width + 1, (height + 1) * image.width + width, capacity=back_weight)
-            G.add_edge((height + 1) * image.width + width + 1, height * image.width + width, capacity=back_weight)
+def graph_get_border(G):
+    print(datetime.now(), "max flow calc")
 
-print(len(G.edges), len(G.nodes), image.width * image.height)
+    res = gt.push_relabel_max_flow(
+    #res = gt.boykov_kolmogorov_max_flow(
+        G,
+        G.vertex(get_node_index(image, 0, image.height)),
+        G.vertex(get_node_index(image, 1, image.height)),
+        G.edge_properties["weight"]
+    )
 
-# connect first column
-for height in range(image.height):
-    G.add_edge(source_index, height * image.width, capacity=back_weight)
+    print(datetime.now(), "min cut calc")
+    partition = gt.min_st_cut(
+        G,
+        G.vertex(get_node_index(image, 0, image.height)),
+        G.edge_properties["weight"],
+        res
+    )
 
-#connect last column
-for height in range(image.height):
-    G.add_edge(height * image.width + image.width - 1, dest_index, capacity=back_weight)
+    print(datetime.now(), "min cut calc finished")
+    # border calculation
 
-cut_value, partition = nx.minimum_cut(G, source_index, dest_index, capacity='capacity')
+    cap = G.edge_properties["weight"]
 
-# border calculation
-edge_nodes = []
-for p1_node in partition[0]:
-    for p2_node in partition[1]:
-        if G.has_edge(p1_node, p2_node):
-            # taking left part
-            edge_nodes.append(p1_node)
+    """
+    res.a = cap.a - res.a 
+    edge_pen_width=gt.prop_to_size(res, mi=1, ma=5, power=0.5)
+    pos = G.new_vertex_property("vector<double>")
+    vprop = {}
 
-print(edge_nodes)
+    for v in G.vertices():
+        if G.vertex_index[v] == image.width * image.height:
+            pos[v] = (0, 10*image.height / 2)
+            continue
+        if G.vertex_index[v] == image.width * image.height + 1:
+            pos[v] = (10*image.width + 20, 10*image.height / 2)
+            continue
+        pos[v] = (G.vertex_index[v] % image.width*10 + 10, 10*math.floor(G.vertex_index[v] / image.width))
+        vprop[v] = f"{G.vertex_index}"
 
-for node in edge_nodes:
-    x = node % image.width
-    y = math.floor(node / image.width)
-    print(x, y)
-    image.putpixel((x, y), (255))
+    G.vertex_properties["pos"] = pos
+
+    gt_draw.graphviz_draw(G, pos=pos, vprops=vprop, pin=True, output="test.png", size=(40,40), vsize=4, penwidth=edge_pen_width, vcolor=partition)
+    """
+
+    left = []
+    border = []
+
+    print(datetime.now(), "sorting")
+
+    for vertex in G.vertices():
+        if partition[vertex]:
+            left.append(G.vertex(vertex))
+    left_sorted = sorted(left, key=lambda node: math.floor(G.vertex_index[node] / image.width))
+    print(datetime.now(), "sorted")
+
+    for vertex in left_sorted: 
+        node = G.vertex_index[vertex]
+        x = node % image.width
+        y = math.floor(node / image.width)
+
+        if len(border) == 0:
+            border.append(node)
+            continue
+
+        x_last = border[-1] % image.width
+        y_last = math.floor(border[-1] / image.width)
+
+        if y >= image.height:
+            continue
+            
+        # exchange
+        if y_last == y:
+            if x_last < x:
+                border[-1] = node
+            continue
+
+        border.append(node)
+
+    return [x % image.width for x in border]
+
+def cut_image_x(image):
+    image = image.convert('L')
+    out_image = Image.new("L", (image.width - 1, image.height))
+    print(datetime.now(), "Graph build")
+    G = build_graph(image)
+    grad = get_gradient_image(image)
+    G = graph_add_weights(G, grad)
+    print(datetime.now(), "Graph build end")
+    border = graph_get_border(G)
+
+    for width in range(image.width):
+        for height in range(image.height):
+            if width == border[height]:
+                continue
+
+            dest_height = height
+            dest_width = width if width < border[height] else width - 1
+            #print(datetime.now(), out_image.width, out_image.height, dest_width, dest_height)
+            out_image.putpixel((dest_width, dest_height), image.getpixel((width, height)))
+    
+    return out_image
+
+
+image = Image.open('banc.jpg')
+
+start_time = datetime.now()
+for i in range(50):
+    image = cut_image_x(image)
+    image.save(f"out/processing-{i}.jpg")
+
+print(datetime.now(), "--- %s seconds ---" % (datetime.now() - start_time))
 
 image.save("test-out.jpg")
-
